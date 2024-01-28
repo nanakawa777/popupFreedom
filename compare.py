b53d7637bde96c8e0d72e4b80cf8868aeb31af53
asuka <asuka.nanakawa@outlook.com>
2024-01-28 22:05:08 +0800

    Refactor compare.py to use semantic search
    Eliminated redundant code related to cosine similarity and refocused on semantic search functionality. This new approach provides similar functionality, but simplifies the codebase, making it easier to maintain and understand.

--- compare.py
+++ compare.py
@@ -1,7 +1,6 @@
 import os.path
 
 from sentence_transformers import SentenceTransformer, util
-from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
 
@@ -17,25 +16,9 @@ else:
 sentence = """如您已完成申报，可忽略此信息"""
 queries = model.encode([sentence], convert_to_tensor=True)
 
-# 执行余弦相似搜索
-similarities = cosine_similarity(queries, sentence_embeddings)
-most_similar_sentence_idx = np.argmax(similarities, )
-cosine_score = round(similarities[0][most_similar_sentence_idx], 3)
-print(
-    f"与【{sentence}】最相似的句子是【{sentences[most_similar_sentence_idx]}】\n"
-    f"相似性得分为【{cosine_score:.3f}】\n"
-)
 
 # 执行语义搜索
-# hit_score = 1
-# hits = util.semantic_search(queries, sentence_embeddings, top_k=1)
-# for hit in hits[0]:
-#     print(f'句子: {sentences[hit["corpus_id"]]}, 相似度得分: {hit["score"]:.3f}')
-#     hit_score = round(hit["score"], 3)
-#     break
-#
-# score = round((cosine_score + hit_score) / 2, 3)
-
-# 打印最相似的句子和相对应的相似性
-if cosine_score < 0.90:
-    print("未找到已配置的相似的规则")
+hits = util.semantic_search(queries, sentence_embeddings)
+for hit in hits[0]:
+    print(f'句子: {sentences[hit["corpus_id"]]}, 相似度得分: {hit["score"]:.3f}')
+    break
