9785c682e5e881aef2f7a928003b13289f433326
lizezheng <lizezheng@yunzhangfang.com>
2024-01-31 17:40:20 +0800

    Update default limit and print messages in get_pending function
    The default limit for the get_pending function is reduced from 100 to 20. More descriptive print statements are added to distinguish between processed and unprocessed alerts. This detailed feedback helps the admin/user to understand the program's status and actions taken.

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ headers = {
 }
 
 
-def get_pending(dqbm: str = "0", limit: str = "100"):
-    """获取待处理的弹框，默认获取全国，前100条"""
+def get_pending(dqbm: str = "0", limit: str = "20"):
+    """获取待处理的弹框，默认获取全国，前20条"""
     response = requests.get("http://taxcontroller.yunzhangfang.com/v_rule/pending_json?page=1&matchkey=",
                             headers=headers,
                             params={"dqbm": dqbm, "limit": limit}
@@ -68,7 +68,7 @@ def run():
             rule_id = semantic_search(content)
             if rule_id:
                 rule = get_publish_rule(rule_id)["data"][0]
-                print(f"套用规则：{rule}")
+                print(f"已处理过的弹框，套用规则：{rule}")
                 new_rule = {
                     'matchtype': rule['matchtype'],
                     'matchkey': content,
@@ -81,6 +81,8 @@ def run():
                     'matchscopevalue': f"{data['dqmc']}-{data['rwlx']}-{data['bslx']}-{data['step']}",
                 }
                 push_new_rule(new_rule)
+            else:
+                print("未处理过的新弹框，跳过")
 
         time.sleep(180)
 
